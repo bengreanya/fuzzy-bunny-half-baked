64895e5ce457ffe7771cb0781da48f1cda847516
Ben Greanya <bengreanya@gmail.com>
2022-11-30 12:36:43 -0800

    got delete function to work

--- create/create.js
+++ create/create.js
@@ -11,14 +11,26 @@ form.addEventListener('submit', async (e) => {
     // use createBunny to create a bunny with this name and family id
     const familyId = formData.get('family-id');
     const name = formData.get('bunny-name');
+    await createBunny({
+        name: name,
+        family_id: familyId,
+    });
     form.reset();
 });
 
 window.addEventListener('load', async () => {
     // let's dynamically fill in the families dropdown from supabase
     // grab the select HTML element from the DOM
+    const select = document.querySelector('select');
     // go get the families from supabase
+    const families = await getFamilies();
     // for each family
+    for (let family of families) {
+        const option = document.createElement('option');
+        option.value = family.id;
+        option.textContent = family.name;
+        select.append(option);
+    }
     // create an option tag
     // set the option's value and text content
     // and append the option to the select
